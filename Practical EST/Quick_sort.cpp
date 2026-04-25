d272644c4535eac78f5db4f3330b7b52f0406c93
BALUMURI VIVEK GOWD <balumurivivek2678@gmail.com>
2026-04-25 14:23:59 +0530

    Implement Quick Sort algorithm in Quick_sort.cpp

---
+++ Practical EST/Quick_sort.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    void quickSort(vector<int>& arr, int low, int high) {
+        if (low < high) {
+            int pIndex = partition(arr, low, high);
+            quickSort(arr, low, pIndex - 1);
+            quickSort(arr, pIndex + 1, high);
+        }
+    }
+
+  public:
+    int partition(vector<int>& arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[i + 1], arr[high]);
+        return i + 1;
+    }
+};
